b50352cb46d88ce0ea8fd004d53913480c41c9e9
Ömer GÜZEL <58399384+omerrguzel@users.noreply.github.com>
2022-10-05 01:39:16 +0300

    Implemented Dagger-Hilt

---
+++ app/src/main/java/com/oguzel/travel_app/HiltApplication.kt
@@ -0,0 +1,12 @@
+package com.oguzel.travel_app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HiltApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
