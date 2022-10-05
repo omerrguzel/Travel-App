6ecfe7934fb2e8dc2b59e97d20cc78da490375cb
Ömer GÜZEL <58399384+omerrguzel@users.noreply.github.com>
2022-10-05 17:33:48 +0300

    Added SearchResultFragment

---
+++ app/src/main/java/com/oguzel/travel_app/SearchResultFragment.kt
@@ -0,0 +1,21 @@
+package com.oguzel.travel_app
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class SearchResultFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_search_result, container, false)
+    }
+}
