6224d594774509f1607aa624d91813cbd1c9613a
Ömer GÜZEL <58399384+omerrguzel@users.noreply.github.com>
2022-10-05 22:20:38 +0300

    Activated functionality of BottomNavView

--- app/src/main/java/com/oguzel/travel_app/MainActivity.kt
+++ app/src/main/java/com/oguzel/travel_app/MainActivity.kt
@@ -1,12 +1,48 @@
 package com.oguzel.travel_app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.oguzel.travel_app.databinding.ActivityMainBinding
+import com.oguzel.travel_app.utils.hide
+import com.oguzel.travel_app.utils.show
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        supportActionBar?.hide()
 
+        val bottomNavigationView : BottomNavigationView = binding.bottomNavigationViewMain
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+        setupWithNavController(bottomNavigationView,navController)
+        appBarConfiguration = AppBarConfiguration(setOf(R.id.homeFragment, R.id.searchFragment, R.id.tripFragment, R.id.guideFragment))
+        setupActionBarWithNavController(navController,appBarConfiguration)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+
+                R.id.detailFragment -> bottomNavigationView.hide()
+                R.id.searchResultFragment -> bottomNavigationView.hide()
+
+                else -> bottomNavigationView.show()
+            }
+        }
     }
+
 }
